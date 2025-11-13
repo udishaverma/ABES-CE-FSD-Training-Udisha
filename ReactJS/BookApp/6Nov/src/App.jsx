0ef899d71ec054435c71fa9b7616196b597b5790
udishaverma <udishaverma13@gmail.com>
2025-11-13 14:13:39 +0530

    commit

--- ReactJS/BookApp/6Nov/src/App.jsx
+++ ReactJS/BookApp/6Nov/src/App.jsx
@@ -12,19 +12,46 @@ const App = () => {
 
   return (
     <div>
-      <Nav />
+      <Nav cartCount={cartCount} />
       <Routes>
-        <Route path="/" element={
-          <>
-            <Book />
-            <div id="cards">
-              <Card img='https://cdn-icons-png.freepik.com/512/9044/9044429.png' title='Maths' price='Rs.400' cartCount={cartCount} setCartCount={setCartCount} />
-              <Card img='https://cdn-icons-png.flaticon.com/512/3277/3277375.png' title='Physics' price='Rs.600' cartCount={cartCount} setCartCount={setCartCount} />
-              <Card img='https://cdn-icons-png.flaticon.com/512/5403/5403827.png' title='Chemistry' price='Rs.900' cartCount={cartCount} setCartCount={setCartCount} />
-              <Card img='https://cdn-icons-png.flaticon.com/512/3038/3038297.png' title='English' price='Rs.800' cartCount={cartCount} setCartCount={setCartCount} />
-            </div>
-          </>
-        } />
+        <Route
+          path="/"
+          element={
+            <>
+              <Book />
+              <div id="cards">
+                <Card
+                  img="https://cdn-icons-png.freepik.com/512/9044/9044429.png"
+                  title="Maths"
+                  price="Rs.400"
+                  cartCount={cartCount}
+                  setCartCount={setCartCount}
+                />
+                <Card
+                  img="https://cdn-icons-png.flaticon.com/512/3277/3277375.png"
+                  title="Physics"
+                  price="Rs.600"
+                  cartCount={cartCount}
+                  setCartCount={setCartCount}
+                />
+                <Card
+                  img="https://cdn-icons-png.flaticon.com/512/5403/5403827.png"
+                  title="Chemistry"
+                  price="Rs.900"
+                  cartCount={cartCount}
+                  setCartCount={setCartCount}
+                />
+                <Card
+                  img="https://cdn-icons-png.flaticon.com/512/3038/3038297.png"
+                  title="English"
+                  price="Rs.800"
+                  cartCount={cartCount}
+                  setCartCount={setCartCount}
+                />
+              </div>
+            </>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Registration />} />
       </Routes>
--- ReactJS/BookApp/6Nov/src/components/Nav.jsx
+++ ReactJS/BookApp/6Nov/src/components/Nav.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css';
 
-function Nav() {
+function Nav({ cartCount }) {
   return (
     <div id='nav'>
       <Link to="/"><button>Home</button></Link>
       <Link to="/login"><button>Login</button></Link>
       <Link to="/register"><button>Register</button></Link>
-      <button>Cart</button>
+      <div className="cart-container">
+        <button>Cart</button>
+        {cartCount > 0 && <span className="cart-badge">{cartCount}</span>}
+      </div>
     </div>
   );
 }
