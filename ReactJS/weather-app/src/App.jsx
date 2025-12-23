98ee23c3abffca77f20197aea90818984fc6e7c5
udishaverma <udishaverma13@gmail.com>
2025-12-23 21:20:40 +0530

    commit

--- ReactJS/weather-app/src/App.jsx
+++ ReactJS/weather-app/src/App.jsx
@@ -5,7 +5,7 @@ import Weather from './components/weather';
 function App() {
   const [city, setCity] = useState('Delhi');
   const [weatherData, setWeatherData] = useState(null);
-  const apiKey = '99d8c48a95c0f58c4d51ec7626fa61c3';
+  const apiKey = '<Your_API_Key>';
 
   useEffect(() => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
